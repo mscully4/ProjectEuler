bf6405eea8e9602ba7b9523d16590ee23e7c5f9e
Michael <michael.scully1997@gmail.com>
2020-07-05 00:41:37 -0400

    Cleaned up a bit

--- Question5/Question5.cpp
+++ Question5/Question5.cpp
@@ -3,21 +3,23 @@
 int main() {
 	using namespace std;
 	int count = 2520;
-	bool run = false;
+	bool run = true;
+	//Any number divisible by these numbers is divisible by 1-10 also
 	vector<int> factors = {20,19,18,17,16,15,14,13,12,11};
-	while (true) {
-		run = true;
+	while (run) {
 		for (auto const& value : factors) {
 			if (count % value != 0) {
 				run = false;
 				break;
 			}
 		}
+
 		if (run) {
 			cout << count << endl;
 			break;
 		} else {
 			++count;
+			run = true;
 		}
 	}
 }
