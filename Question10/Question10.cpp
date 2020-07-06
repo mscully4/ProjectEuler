77d649d34d921b3290ef0e35850b17c9b6a91eb1
Michael <michael.scully1997@gmail.com>
2020-07-05 22:36:27 -0400

    Cleaned up 10

--- Question10/Question10.cpp
+++ Question10/Question10.cpp
@@ -8,14 +8,16 @@ int main () {
 	double root;
 	bool prime;
 	for (int i = 3; i < 2000000; i += 2) {
-		bool prime = true;
+		prime = true;
 		root = sqrt(i);
+		//If the number has a clean square root, it obviously isn't prime
 		if (root != (int)root) {
 			for (int x = 2; x < root; x++) {
 				if (i % x == 0) {
 					prime = false;
 				}
 			}
+			//if nothing divides cleanly into the number, then add it to the sum
 			if (prime) {
 				sum += i;
 			}
